50fcda31cb9033a68988950e6c26f20f5641f944
pedrodelosreyes <pedro.reyes@ibvf.csic.es>
2022-03-17 18:39:02 +0100

    Modifying quantification for paired or single end data

--- quantification.sh
+++ quantification.sh
@@ -11,5 +11,13 @@
 cd /home/pedro/arabidopsis/rnaseq_studies/co_rnaseq_2022/co_rnaseq_14dag_v2/samples/bams
 
 ## Gene Expression Quantification
-featureCounts -O -M -a ../../annotation/annotation.gtf -o counts.txt $(ls *.bam) -s 2 --verbose
+
+#single end
+#featureCounts -O -a ../../annotation/annotation.gtf -o counts.txt $(ls *.bam) -s 2 --verbose
+
+#Paired end
+featureCounts -O -p --countReadPairs -a ../../annotation/annotation.gtf -o counts.txt $(ls *.bam) -s 2 --verbose
+
+#You have to modify the -s parameter depending on the strandness of your library
+
 
